f512a6371fea87154c38029cf995e278bbc646e0
jmel01 <jmelhistorillo@gmail.com>
2021-05-27 19:10:07 +0800

    Fixed user active status

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -6,6 +6,22 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jszip-2.5.0/dt-1.10.23/af-2.3.5/b-1.6.5/b-colvis-1.6.5/b-flash-1.6.5/b-html5-1.6.5/b-print-1.6.5/cr-1.5.3/fc-3.3.2/fh-3.1.7/kt-2.5.3/r-2.2.6/rg-1.1.2/rr-1.2.7/sc-2.0.3/sb-1.0.1/sp-1.2.2/sl-1.3.1/datatables.min.css" />
 <link rel="stylesheet" href="{{ asset('/bower_components/admin-lte/plugins/select2/css/select2.min.css') }}">
 <link rel="stylesheet" href="{{ asset('/bower_components/admin-lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css') }}">
+<style>
+    .icon-container {
+        position: relative;
+    }
+
+    .status-circle {
+        width: 12px;
+        height: 12px;
+        border-radius: 50%;
+        background-color: #28a745;
+        border: 2px solid white;
+        bottom: 0;
+        right: 0;
+        position: absolute;
+    }
+</style>
 @endpush
 
 @section('content')
@@ -35,12 +51,12 @@
                 @foreach ($data as $key => $user)
                 <tr>
                     <td>
-                        <div class="user-block">
+                        <div class="user-block icon-container">
                             <img src="/storage/users-avatar/{{$user->avatar}}" class="img-circle img-bordered-sm cover" alt="User Image">
                             @if($user->active_status)
-                                <i class="fa fa-circle text-success text-xs"></i>
+                                <div class='status-circle'>
                             @endif
-                        </div>                        
+                        </div>
                     </td>
                     <td> {{ $user->name }}</td>
                     <td>
